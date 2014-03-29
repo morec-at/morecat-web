fcf3e3bf0d1159b0a6a142adde0aa3ebab250b03
emag <lanabe.lanabe@gmail.com>
2014-03-29 17:22:51 +0900

    Add BlogCtrl

--- app/modules/contents/blog/blog.js
+++ app/modules/contents/blog/blog.js
@@ -6,7 +6,19 @@ blog.config(['$routeProvider', function config($routeProvider) {
 
   $routeProvider
     .when('/blog', {
-      templateUrl: 'assets/partials/contents/blog/blogTmpl.html'
+      templateUrl: 'assets/partials/contents/blog/blogTmpl.html',
+      controller: 'BlogCtrl'
     });
 
 }]);
+
+blog.controller('BlogCtrl', function($scope) {
+  $scope.entries = [
+    {'title': 'Title1',
+      'content': '<p>Content1</p>'},
+    {'title': 'Title2',
+      'content': '<p>Content2</p>'},
+    {'title': 'Title3',
+      'content': '<p>Content3</p>'}
+  ];
+});
