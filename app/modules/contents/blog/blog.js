3d0d26d42434ac2bd74098e184542d71ec41f1df
emag <lanabe.lanabe@gmail.com>
2014-04-01 21:31:45 +0900

    Change Blog as Home and Remove Home view

--- app/modules/contents/blog/blog.js
+++ app/modules/contents/blog/blog.js
@@ -5,20 +5,15 @@ var blog = angular.module('blog', []);
 blog.config(['$routeProvider', function config($routeProvider) {
 
   $routeProvider
-    .when('/blog', {
+    .when('/', {
       templateUrl: 'assets/partials/contents/blog/blogTmpl.html',
       controller: 'BlogCtrl'
     });
 
 }]);
 
-blog.controller('BlogCtrl', function($scope) {
-  $scope.entries = [
-    {'title': 'Title1',
-      'content': '<p>Content1</p>'},
-    {'title': 'Title2',
-      'content': '<p>Content2</p>'},
-    {'title': 'Title3',
-      'content': '<p>Content3</p>'}
-  ];
+blog.controller('BlogCtrl', function($scope, $http) {
+  $http.get('http://localhost:8080/morecat/api/entries/').success(function(entries) {
+    $scope.entries = entries;
+  });
 });
--- app/modules/contents/contents.js
+++ app/modules/contents/contents.js
@@ -1,6 +1,6 @@
 'use strict';
 
-var contents = angular.module('contents', ['ngRoute', 'home', 'about', 'blog']);
+var contents = angular.module('contents', ['ngRoute', 'about', 'blog']);
 
 contents.config(['$locationProvider', function config($locationProvider) {
   $locationProvider.html5Mode(true).hashPrefix('!');
