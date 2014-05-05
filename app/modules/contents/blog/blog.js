da6b1779cd1fcf9a2900187f8ae77e564810b4ed
emag <lanabe.lanabe@gmail.com>
2014-05-06 06:29:32 +0900

    Sorted blog archive

--- app/modules/contents/blog/blog.js
+++ app/modules/contents/blog/blog.js
@@ -14,12 +14,21 @@ blog.config(['$routeProvider', function config($routeProvider) {
 
 blog.controller('BlogCtrl', ['$scope', '$http', function($scope, $http) {
   $http.get('http://localhost:8080/morecat/api/entries/').success(function(entries) {
-    $scope.entries = entries;
     _.each(entries, function(entry) {
       entry.year = new Date(entry.createdDate).getFullYear();
       entry.month = new Date(entry.createdDate).getMonth() + 1;
       entry.day = new Date(entry.createdDate).getDate();
     });
+    var years = _.groupBy(entries, function(entry) {
+      return new Date(entry.createdDate).getFullYear();
+    });
+    years = _.each(years, function(year, key) {
+      year.year = key;
+    });
+    $scope.years = _.sortBy(years, function(year, key) {
+      return -key;
+    });
+    console.log($scope.years);
   });
   $http.get('http://localhost:8080/morecat/api/entries/tags').success(function(tags) {
     $scope.tags = tags;
